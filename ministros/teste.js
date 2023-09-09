538c3718a08fa1e24c4163463d0f12b5116d54af
RONILDO <124939567+459352578@users.noreply.github.com>
2023-09-08 21:42:44 -0300

    Add files via upload
    Iniciado site de ministros da sagrada comuunhao tatuquara

---
+++ ministros/teste.js
@@ -0,0 +1,5 @@
+document.getElementById("demo").innerHTML = "Ministros da Paróquia Sagrado Coração de Jesus";
+
+document.getElementById("demo").style.fontSize = "35px";
+document.getElementById("demo").style.display = "block";
+document.getElementById("demo").style.display = "none";
